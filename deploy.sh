ad6b291e0b7e54228856f25f5c79748751002501
Dylan Davies <ddavies@orbiscommunications.com>
2020-01-17 11:07:25 -0500

    Readying for GitHub pages

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env sh
 
-# abort on errors
 set -e
 
-# build
 npm run build
 
-# navigate into the build output directory
 cd docs/.vuepress/dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
 
 git init
 git add -A
