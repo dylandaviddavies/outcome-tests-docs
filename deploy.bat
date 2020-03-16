98924451b4a525de291652c401fb6612351315e2
Dylan Davies <49349823+dylandaviddavies@users.noreply.github.com>
2020-03-16 14:46:16 -0400

    Updated deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,9 +1,6 @@
-npm run build
-
-cd docs/.vuepress/dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-git push -f git@github.com:dylandaviddavies/outcome-tests-docs.git master:gh-pages
+call npm run build
+call cd docs/.vuepress/dist
+call git init
+call git add -A
+call git commit -m 'deploy'
+call git push -f git@github.com:dylandaviddavies/outcome-tests-docs.git master:gh-pages
